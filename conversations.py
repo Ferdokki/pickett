8280254081440d2e3d85a18ed23599191b414617
lerugray <lerugray@gmail.com>
2013-12-14 18:24:05 -0500

    Started preliminary work on Conversation Class

--- conversations.py
+++ conversations.py
@@ -2,3 +2,8 @@ import characters
 import messages
 import random
 
+class Conversation(object):
+  """Abstract base class for all conversations in game."""
+  def __init__(self, npc, answer=None):
+          self.npc = npc
+          self.answer = answer
