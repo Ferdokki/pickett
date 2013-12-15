7328e024c8d92e5598f4c9eee3d2c8b7f52a6ef5
lerugray <lerugray@gmail.com>
2013-12-14 20:39:58 -0500

    Added method print_conversations and started TROID_OUSIDE_THEATER

--- conversations.py
+++ conversations.py
@@ -4,6 +4,17 @@ import random
 
 class Conversation(object):
   """Abstract base class for all conversations in game."""
-  def __init__(self, npc, answer=None):
-          self.npc = npc
-          self.answer = answer
+
+  def __init__(self, npc, scene, answer=None):
+    self.npc = npc
+    self.answer = answer
+  
+  def print_conversations(conversations):
+    """Print a bunch of conversations."""
+    for c in conversations:
+      raw_input(m + messages.ENTER)
+
+  TROID_OUTSIDE_THEATER = ('''
+Troid:
+    Uh, hey. Whats up
+""")
