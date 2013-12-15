1032bb1768d7fbef56d94baea07f6783dec06e3a
lerugray <lerugray@gmail.com>
2013-12-15 03:04:07 -0500

    Making progress on conversations.py

--- conversations.py
+++ conversations.py
@@ -12,10 +12,20 @@ class Conversation(object):
   def outside_theater_day_one(self.npc):
     """Prints and processes conversations outside
     the theater after orientation on Day One."""
+
     if self.npc = 'Troid':
-      raw_input = (
+      npc_speaking = '\n%s:\n\t'
+      raw_input(
         'You see the Counselor from the bus. '    
         'He is wearing a backwards multi-colored hat, '
         't-shirt with a soda, shorts, big sneakears, '
         'and a small cigarette hanging from his mouth.'
-        + messages.ENTER) 
+        + messages.ENTER)
+
+      raw_input(npc_speaking + 'Hey Bro.' + messages.ENTER) % self.npc
+
+      self.answer = raw_input(npc_speaking + 'Oh man, it\'s hot, no ' 
+      'ladies here either, do you like soda?') #need to finish stuff here.
+
+                              
+                
