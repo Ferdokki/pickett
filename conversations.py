5bcb3fe291f55463a970f550cfd8c8e7bdbf69c0
lerugray <lerugray@gmail.com>
2013-12-15 01:29:41 -0500

    Added characters txt, worked more on conversations.py

--- conversations.py
+++ conversations.py
@@ -5,16 +5,17 @@ import random
 class Conversation(object):
   """Abstract base class for all conversations in game."""
 
-  def __init__(self, npc, scene, answer=None):
+  def __init__(self, npc, answer=None):
     self.npc = npc
     self.answer = answer
-  
-  def print_conversations(conversations):
-    """Print a bunch of conversations."""
-    for c in conversations:
-      raw_input(m + messages.ENTER)
 
-  TROID_OUTSIDE_THEATER = ('''
-Troid:
-    Uh, hey. Whats up
-''')
+  def outside_theater_day_one(self.npc):
+    """Prints and processes conversations outside
+    the theater after orientation on Day One."""
+    if self.npc = 'Troid':
+      raw_input = (
+        'You see the Counselor from the bus. '    
+        'He is wearing a backwards multi-colored hat, '
+        't-shirt with a soda, shorts, big sneakears, '
+        'and a small cigarette hanging from his mouth.'
+        + messages.ENTER) 
