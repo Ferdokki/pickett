03c82caab069bbfea49378f6d4297ddab0973e99
Tom Ritchford <tom@swirly.com>
2014-01-24 12:39:41 -0500

    Move some stuff around...

--- pickett/Scenes.py
+++ pickett/Scenes.py
@@ -1,9 +1,8 @@
 import pickett
-import 
 
 class Scene(object):
   """Keeps track of scene variables)"""
   def __init__(self, convo_flag=0, characters_talked_to=0):
     self.convo_flag = convo_flag
     self.characters_talked_to = characters_talked_to
-  
+
